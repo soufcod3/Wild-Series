36fb7a8f0912697ee74c95984b7bb44f724a234e
Soufiane <soufwra@gmail.com>
2021-12-04 21:25:12 +0100

    quest 16 EntityType

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+
 
 class ProgramType extends AbstractType
 {
@@ -18,7 +22,18 @@ class ProgramType extends AbstractType
             ->add('country')
             ->add('year')
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+            'class' => Actor::class,
+            //Return of getSelector() method in Entity\Actor
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => false,
+            'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('u')
+                    ->orderBy('u.name', 'ASC');
+            },
+            'by_reference' => false,]
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
