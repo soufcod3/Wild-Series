eeb45a03b069edf209d6372bd1640b5433415fbf
Soufiane <soufwra@gmail.com>
2021-12-04 14:32:52 +0100

    quest 13 crud

---
+++ migrations/Version20211204121231.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211204121231 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE episode_program (episode_id INT NOT NULL, program_id INT NOT NULL, INDEX IDX_18E483F7362B62A0 (episode_id), INDEX IDX_18E483F73EB8070A (program_id), PRIMARY KEY(episode_id, program_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE episode_program ADD CONSTRAINT FK_18E483F7362B62A0 FOREIGN KEY (episode_id) REFERENCES episode (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE episode_program ADD CONSTRAINT FK_18E483F73EB8070A FOREIGN KEY (program_id) REFERENCES program (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE episode_program');
+    }
+}
